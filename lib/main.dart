5e5929f9b7a4e1cd2834a0d4febca15c4aeefeb0
OpsonDuran <iccarmandoduran@gmail.com>
2019-06-11 21:54:07 -0500

    add new option of navigation in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components_wigets_use_examples/src/widgets/alert_page.dart';
+import 'package:flutter_components_wigets_use_examples/src/widgets/avatar_file.dart';
 import 'package:flutter_components_wigets_use_examples/src/widgets/home_page.dart';
-//import 'package:flutter_components_wigets_use_examples/src/widgets/home_temp.dart';
- 
+
+
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
@@ -10,7 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePage()
+      routes: <String,WidgetBuilder>{
+        '/' : (BuildContext context) => HomePage(),
+        'alert' : (BuildContext context) => AlertPage(),
+        'avatar' : (BuildContext context) => AvatarPage(),
+      },
     );
   }
 }
